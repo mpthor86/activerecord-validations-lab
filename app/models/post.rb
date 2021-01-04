e2439650969ce2569ad14502c418872df92bf6d0
michael <mpthornton86@gmail.com>
2021-01-04 13:50:00 -0700

    commit tree 0249c577e56613395f1f3c9f709914e5f014e9dc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :click_bait?
+
+    @@clickbait = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+
+    def click_bait?
+        if @@clickbait.none? {|bait| bait.match title}
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
